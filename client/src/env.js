0b766e26176380853b16ec16f82873db6be00884
Samuel Morgan <sam2morgan99@gmail.com>
2024-03-28 16:35:21 -0600

    tables created

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-t4wakbltjiab2t5f.us.auth0.com'
+export const clientId = 'FZXb1zStlqaiSGHQgxilPLGHbrxkbnk6'
+export const audience = 'https://thisisathingy.com'
