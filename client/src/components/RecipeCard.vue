27ed959556c9b34636627b6cb2d1458652d88257
Samuel Morgan <sam2morgan99@gmail.com>
2024-04-01 16:17:36 -0600

    css struggles

---
+++ client/src/components/RecipeCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col-4">
+        <div class="card text-white bg-warning">
+            <img class="card-img-top" src="" alt="Title" />
+            <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    setup() {
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12 d-flex background-img">
+        <p class="display-5 m-auto text-white">Cherish Your Beans</p>
+      </div>
+    </section>
+  </div>
+  <div class="container">
+    <section class="row">
+      <RecipeCard />
+    </section>
   </div>
 </template>
 
@@ -14,30 +17,18 @@
 export default {
   setup() {
     return {
-      
+
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.background-img {
+  background-image: url(https://images.unsplash.com/photo-1564149504298-00c351fd7f16?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-position: center;
+  position: absolute;
+  top: 0px;
+  bottom: 300px;
 }
 </style>
